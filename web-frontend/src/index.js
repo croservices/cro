7ddb6908d4fc979dd5f101f8779ddb5b5c120fda
Jonathan Worthington <jnthn@jnthn.net>
2017-08-16 00:51:18 +0200

    Stub in build toolchain for Cro web frontend.

---
+++ web-frontend/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+var MainArea = props => <div>Cro Web Tool</div>;
+
+ReactDOM.render(
+    <MainArea />,
+    document.getElementById('app')
+);
