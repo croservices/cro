5e525bee96eb8a05f7364984257fa95cb06e67ab
Jonathan Worthington <jnthn@jnthn.net>
2017-08-22 12:53:48 +0200

    Render navigation bar using react-bootstrap.

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -1,7 +1,29 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-var MainArea = props => <div>Cro Web Tool</div>;
+var MainArea = props => (
+    <div>
+      <Navigation />
+      <div className="container">
+      </div>
+    </div>
+);
+
+var Navigation = props => (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="/">Cro Development Tool</a>
+        </Navbar.Brand>
+        </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">Dashboard</NavItem>
+        <NavItem eventKey={2} href="#">Stub Service</NavItem>
+        <NavItem eventKey={3} href="http://cro.services/docs">Documentation</NavItem>
+      </Nav>
+    </Navbar>
+);
 
 ReactDOM.render(
     <MainArea />,
