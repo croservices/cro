0c6450b9b1bc2b73be57c50388dc0e0448750f23
Altai-man <altay-man@mail.ru>
2017-10-05 22:29:24 +0300

    Fix reducer and implement SERVICE_STARTED signal

--- web-frontend/src/service-list/reducer.js
+++ web-frontend/src/service-list/reducer.js
@@ -1,16 +1,21 @@
-import $ from 'jquery';
-import ActionTypes from './actions';
+import * as ActionTypes from './actions';
 
 const initialState = {
     services: new Map()
 }
 
-export function serviceListReducer(state = initialState, action) {
+export default function serviceListReducer(state = initialState, action) {
     switch (action.type) {
-    case Action.Types.SERVICE_START:
+    case ActionTypes.SERVICE_START:
         let updated_services = state.services;
         updated_services.get(action.id).status = 'Started';
         return { ...state, services: updated_services }
+    case ActionTypes.SERVICE_STARTED:
+        let service = { name: action.name };
+        let new_services = state.services;
+        new_services.set(service.name, service);
+        return { ...state, services: new_services };
+    default:
+        return state;
     }
-    
 }
